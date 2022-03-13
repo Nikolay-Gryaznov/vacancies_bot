5f2a9df75ec9cf8a22f2001ad3afd50fcc69be01
StaroePovidlo <gryaznownikolay@gmail.com>
2022-03-13 20:36:58 +0300

    Add VarConstants

---
+++ src/main/java/ru/gryaznov/bot/constants/VarConstants.java
@@ -0,0 +1,67 @@
+package ru.gryaznov.bot.constants;
+
+public class VarConstants {
+    public static final String START = "/start";
+    public static final String GREETING_MESSAGE = "Привет, здесь можно найти различные вакансии!\n"
+            + "Для начала Вы можете заполнить акнкету для поиска вакансий, но это не обязательная процедура,"
+            + " так же как и заполнение всех полей.\n"
+            + "Для поиска вакансий нажмите \"Найти вакансии\", для дальнейшего поиска продолжайте нажимать \"Найти вакансии\"\n"
+            + "Если возникают проблемы с заполнением анкеты, воспользуйтесь командой /start";
+
+    public static final String SHOW_PERON = "Моя анкета";
+    public static final String CLEAR_PERSON = "Очистить анкету";
+
+    public static final String SEARCH_VACANCIES = "Найти вакансии";
+    public static final String SEARCH_SETTINGS = "Настроить поиск";
+    public static final String MAIN_MENU = "Главное меню";
+
+    public static final String SETTING_MESSAGE = "Выберите параметр поиска";
+
+    public static final String SHOW_JOB_NAME = "Название профессии";
+    public static final String CHANGE_JOB_NAME = "Изменить вакансию";
+
+    public static final String SHOW_AREA = "Регион";
+    public static final String CHANGE_AREA = "Изменить регион";
+
+    public static final String SHOW_EXPERIENCE = "Опыт работы";
+    public static final String CHANGE_EXPERIENCE = "Изменить опыт работы";
+    public static final String NO_EXPERIENCE = "Без опыта";
+    public static final String EXPERIENCE_BETWEEN1AND3 = "От 1 до 3 лет";
+    public static final String EXPERIENCE_BETWEEN3AND6 = "От 3 до 6 лет";
+    public static final String EXPERIENCE_MORE_THAN6 = "Более 6 лет";
+
+    public static final String SHOW_SCHEDULE = "График работы";
+    public static final String CHANGE_SCHEDULE = "Изменить график работы";
+    public static final String FULL_DAY = "Полный день";
+    public static final String SHIFT = "Сменный график";
+    public static final String FLEXIBLE = "Гибкий график";
+    public static final String REMOTE = "Удалённая работа";
+    public static final String FLY_IN_FLY_OUT = "Вахтовый метод";
+
+    public static final String SHOW_EMPLOYMENT = "Тип занятости";
+    public static final String CHANGE_EMPLOYMENT = "Изменить тип занятости";
+    public static final String FULL = "Полная занятость";
+    public static final String PART = "Частичная занятость";
+    public static final String PROJECT = "Проектная работа";
+    public static final String VOLUNTEER = "Волонтерство";
+    public static final String PROBATION = "Стажировка";
+
+    public static final String SHOW_SALARY = "Доход";
+    public static final String CHANGE_SALARY = "Изменить доход";
+
+    public static final String SHOW_SORTING = "Тип сортировки";
+    public static final String CHANGE_SORTING = "Изменить тип сортировки";
+    public static final String PUBLICATION_DATA = "По дате";
+    public static final String SALARY_DESC = "По убыванию дохода";
+    public static final String SALARY_ASC = "По возрастанию дохода";
+
+    public static final String BACK = "Назад";
+
+    public static final String TAG_NAME = "name";
+    public static final String TAG_ID = "id";
+    public static final String TAG_AREAS = "areas";
+
+
+
+
+}
